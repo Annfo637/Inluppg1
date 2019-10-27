4a3ff8a0cf722ba0be402fc0df3c7bf828b779a3
Annelie Fogel 2019 <anneliefogel2019@AnneliesAir2019.lan>
2019-10-27 11:50:04 +0100

    Added some style and winMsg

--- main.js
+++ main.js
@@ -1,22 +1,29 @@
-const boardSize = 10
+const boardSize = prompt('Choose size of the board with a number between 10 and 40:')
 const currentPlayer = document.querySelector("#currentPlayer")
 const totalMoves = document.querySelector("#countMoves")
 const gameBoard = document.querySelector(".boardframe")
+const parentBoard = document.querySelector("#parentboard")
 const p1symbol = "😼"
 const p2symbol = "👻"
 let numberMoves = 0
+const boardArray = [[]]
+
+
 
 //skapa bräde med ID och lyssnare för varje knapp
 for(let x=0; x<boardSize; x++){
-    let newRow = document.createElement('div')
-    newRow.classList.add('row')
+    const newRow = document.createElement("div")
+    newRow.classList.add("row")
     gameBoard.appendChild(newRow)
+    boardArray[x] = newRow
     for(let y=0; y<boardSize; y++){
         const newBtn = document.createElement("button")
-        newBtn.id =`${x},${y}`
-        newBtn.classList.add('button')
-        newBtn.addEventListener('click', btnClick)
+        newBtn.dataset.row = x
+        newBtn.dataset.col = y
+        newBtn.classList.add("button")
+        newBtn.addEventListener("click", btnClick)
         newRow.appendChild(newBtn)
+        boardArray[x][y] = newBtn
     } 
 }
 //lyssna efter klick och rita ut drag
@@ -31,34 +38,41 @@ function btnClick(event) {
             currentBtn.textContent = p1symbol
             currentPlayer.classList.replace("player1", "player2")
             currentPlayer.textContent = "It's your turn, player 👻"
-            numberMoves += 1
+            numberMoves++
             totalMoves.textContent = `${numberMoves} moves in total`
+            if(checkWin(currentBtn)){
+                announceWinner(p1symbol)
+            }
         }
         else{
             currentBtn.textContent = p2symbol
             currentPlayer.classList.replace("player2", "player1")
             currentPlayer.textContent = "It's your turn, player 😼"
-            numberMoves += 1
+            numberMoves++
             totalMoves.textContent = `${numberMoves} moves in total`
+            if(checkWin(currentBtn)){
+                announceWinner(p2symbol)
+            }
         }
-        checkWin(currentBtn)
     }
 }
 
 //kontrollera för vinst
 function checkWin(clickedBtn){
-const clickedRow = clickedBtn.dataset.row
-const clickedCol = clickedBtn.dataset.col
-const clickedSymb = clickedBtn.textContent
+    console.log(clickedBtn)
+    const clickedRow = clickedBtn.dataset.row
+    const clickedCol = clickedBtn.dataset.col
+    const clickedSymb = clickedBtn.textContent
+    console.log(clickedRow, clickedCol, clickedSymb)
 
    if(horizontalWin(clickedRow, clickedCol, clickedSymb)===true ||
-    verticalWin(clickedRow, clickedCol, clickedSymb)===true ||
-    topdownWin(clickedRow, clickedCol, clickedSymb)===true ||
-    bottomupWin(clickedRow, clickedCol, clickedSymb)===true){
-        alert("You won!")
+   verticalWin(clickedRow, clickedCol, clickedSymb)===true ||
+   topdownWin(clickedRow, clickedCol, clickedSymb)===true ||
+   bottomupWin(clickedRow, clickedCol, clickedSymb)===true){
+        return true
     }   
     else{
-            return false 
+        return false 
         }
 }
 
@@ -69,7 +83,7 @@ function horizontalWin(row, col, symb){
         
     for(let i=-4; i<5; i++){
         const checkBtn = boardArray[r][c+i]
-         console.log(checkBtn)
+        console.log(checkBtn)
           
         if(typeof(checkBtn) !== 'undefined'){
             if(checkBtn.textContent === symb){
@@ -93,8 +107,9 @@ function verticalWin(row, col, symb){
     let c = parseInt(col)
         
     for(let i=-4; i<5; i++){
+        console.log('loop', i)
         const checkBtn = boardArray[r+i][c]
-         console.log(checkBtn)
+        console.log(checkBtn)
           
         if(typeof(checkBtn) !== 'undefined'){
             if(checkBtn.textContent === symb){
@@ -160,4 +175,13 @@ function bottomupWin(row, col, symb){
             }
         }                   
     }            
+}
+
+function announceWinner(player){
+    const winMsg = document.createElement("p")
+    winMsg.classList.add('winnerMsg')
+    winMsg.textContent = `Congratulations, player ${player}!
+    Refresh page for a new game.`
+    parentBoard.appendChild(winMsg)
+    gameBoard.classList.add("noshow")
 }
